3965311497b4d3d02378704e81e96cf1d9428bcf
Adrian Netto <adrian.netto73@gmail.com>
2024-01-26 11:17:12 -0300

    solve migration error

--- src/shared/typeorm/migrations/1706214578877-CreateProducts.ts
+++ src/shared/typeorm/migrations/1706214578877-CreateProducts.ts
@@ -11,7 +11,7 @@ export class CreateProducts1706214578877 implements MigrationInterface {
             type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
-            default: 'uuid__generate_v4()',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'name',
