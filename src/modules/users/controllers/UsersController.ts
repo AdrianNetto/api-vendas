3e66d22567e4a38c8844258a24cd9eae4ac8dede
Adrian Netto <adrian.netto73@gmail.com>
2024-02-02 11:00:28 -0300

    users

---
+++ src/modules/users/controllers/UsersController.ts
@@ -0,0 +1,27 @@
+import { Request, Response, response } from 'express';
+import ListUserService from '../services/ListUSersService';
+import CreateUserService from '../services/CreateUserService';
+
+export default class UsersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listUser = new ListUserService();
+
+    const users = await listUser.execute();
+
+    return response.json(users);
+  }
+
+  public async create(request: Request): Promise<Response> {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
+  }
+}
