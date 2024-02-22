847c75cb3c5a27545481c99d60326d9cd9e9ad64
Adrian Netto <adrian.netto73@gmail.com>
2024-02-22 10:52:52 -0300

    feat: adding anti ddos and brute force attacks

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-import 'dotenv/config'
+import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
@@ -9,11 +9,13 @@ import '@shared/typeorm';
 import { errors } from 'celebrate';
 import { pagination } from 'typeorm-pagination';
 import uploadConfig from '@config/upload';
+import rateLimiter from '@shared/http/middlewares/rateLimiter';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(rateLimiter);
 app.use(pagination);
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
