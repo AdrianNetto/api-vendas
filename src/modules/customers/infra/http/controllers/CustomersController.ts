632db7e11ef48ae82f8e01b7acbc0a76017352e6
Adrian Netto <adrian.netto73@gmail.com>
2024-03-06 10:27:53 -0300

    feat: Dependency inversion

--- src/modules/customers/infra/http/controllers/CustomersController.ts
+++ src/modules/customers/infra/http/controllers/CustomersController.ts
@@ -4,6 +4,7 @@ import ShowCustomerService from '../../../services/ShowCustomerService';
 import CreateCustomerService from '../../../services/CreateCustomerService';
 import UpdateCustomerService from '../../../services/UpdateCustomerService';
 import DeleteCustomerService from '../../../services/DeleteCustomerService';
+import CustomersRepository from '../../typeorm/repositories/CustomersRepository';
 
 export default class CustomersController {
   public async index(request: Request, response: Response): Promise<Response> {
@@ -27,7 +28,9 @@ export default class CustomersController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { name, email } = request.body;
 
-    const createCustomer = new CreateCustomerService();
+    const custoemersRepository = new CustomersRepository();
+
+    const createCustomer = new CreateCustomerService(custoemersRepository);
 
     const customer = await createCustomer.execute({
       name,
--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -2,14 +2,16 @@ import { getCustomRepository } from 'typeorm';
 import AppError from '@shared/errors/AppError';
 import Customer from '../infra/typeorm/entities/Customer';
 import CustomersRepository from '../infra/typeorm/repositories/CustomersRepository';
-
-interface IRequest {
-  name: string;
-  email: string;
-}
+import { ICustomersRepository } from '../domain/repositories/ICustomersRepository';
+import { ICreateCustomer } from '../domain/models/ICreateCustomer';
+import { ICustomer } from '../domain/models/ICustomer';
 
 class CreateUserService {
-  public async execute({ name, email }: IRequest): Promise<Customer> {
+  constructor(private customersRepository: ICustomersRepository) {
+
+  }
+
+  public async execute({ name, email }: ICreateCustomer): Promise<ICustomer> {
     const customersRepository = getCustomRepository(CustomersRepository);
     const emailExists = await customersRepository.findByEmail(email);
 
@@ -17,12 +19,10 @@ class CreateUserService {
       throw new AppError('This email is already in use');
     }
 
-    const customer = customersRepository.create({
+    const customer = this.customersRepository.create({
       name,
       email,
     });
-
-    await customersRepository.save(customer);
 
     return customer;
   }
